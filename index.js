edeed077aed778e444cc0b117f4d34b201bfc96b
shubhamwhite <shubham@whitelotuscorporation.com>
2024-11-28 17:30:50 +0530

    msg: test pipeline

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require('./src/config/general.config')
 const clc = require('./src/helper/cliColor.helper')
 const connectToDb = require('./src/models')
 
+// Port connection
 app.listen(config.PORT, (error) => {
   connectToDb()
   const PORT = config.PORT
