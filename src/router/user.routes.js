890a46d3097fccdb87608ffdcb1f0638c91a556e
shubhamwhite <shubham@whitelotuscorporation.com>
2024-11-28 11:44:46 +0530

    husky setupd

--- src/router/user.routes.js
+++ src/router/user.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const validateUser = require('../middlewares/user.validation')
 const errorMiddleware = require('../middlewares/error.middleware')
-const { registration } = require('../controller/user.controller')
+ const { registration } = require('../controller/user.controller')
 
  router.route('/register').post(validateUser, registration)
 
