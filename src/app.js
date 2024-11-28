c58e96f2b0374aaccbd4ad621149443889a33124
shubhamwhite <shubham@whitelotuscorporation.com>
2024-11-28 11:31:32 +0530

    husky setup

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/v1/user', require('../src/router/user.routes'))
 
-   app.get('/', (req, res) => {
+  app.get('/', (req, res) => {
   res.json({ message: 'testing application' })
 })
 
